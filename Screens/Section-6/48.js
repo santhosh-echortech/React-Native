263ecdc927c573e58f33577af9191e9a5e750c98
Santhosh Kumar <santhosh@echortech.com>
2023-04-14 17:18:15 +0530

    Color Screen

--- Screens/Section-6/48.js
+++ Screens/Section-6/48.js
@@ -1,15 +1,58 @@
-import React from 'react'
-import { View,StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native'
 
 function ColorScreen() {
-    return(
-        <View>
-            <View/>
+    const [red, setRed] = useState(0)
+    const [green, setGreen] = useState(0)
+    const [blue, setBlue] = useState(0)
+
+    const ColorCounter = ({ color, onIncrease, onDecrease }) => {
+        return (
+            <>
+                <Text style={{ textAlign: 'center', marginTop: '5%' }}>{color}</Text>
+                <TouchableOpacity onPress={onIncrease} style={styles.button} activeOpacity={0.7}>
+                    <Text style={styles.buttonText}>{`More ${color}`}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onDecrease} style={styles.button} activeOpacity={0.7}>
+                    <Text style={styles.buttonText}>{`Less ${color}`}</Text>
+                </TouchableOpacity>
+            </>
+        )
+    }
+    console.log(`rgb(${red},${green},${blue})`)
+    return (
+        <View style={styles.container}>
+            <ColorCounter onIncrease={() => setRed(red + 10)} onDecrease={() => setRed(red - 10)} color='Red' />
+            <ColorCounter onIncrease={() => setGreen(green + 10)} onDecrease={() => setGreen(green - 1)} color='Green ' />
+            <ColorCounter onIncrease={() => setBlue(blue + 10)} onDecrease={() => setBlue(blue - 10)} color='Blue' />
+            <View style={[styles.colorContainer, { backgroundColor: `rgb(${red},${green},${blue})` }]} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-  
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    colorContainer: {
+        height: 100,
+        width: 100,
+        marginTop: '5%'
+    },
+    button: {
+        height: 50,
+        width: '95%',
+        alignSelf: 'center',
+        backgroundColor: 'lightblue',
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '5%'
+    },
+    buttonText: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
 })
 export default ColorScreen
