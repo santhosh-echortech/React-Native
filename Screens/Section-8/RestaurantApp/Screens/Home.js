b83dadab12bef7e61df57940722a11feda343f2e
Santhosh Kumar <santhosh@echortech.com>
2023-04-20 14:48:00 +0530

    Brewery Listing App

--- Screens/Section-8/RestaurantApp/Screens/Home.js
+++ Screens/Section-8/RestaurantApp/Screens/Home.js
@@ -1,30 +1,67 @@
-import React, { useState } from "react"
-import { View, Text, StyleSheet, Image, StatusBar, TextInput, ScrollView } from 'react-native'
+import React, { useEffect, useState } from "react"
+import { View, Text, StyleSheet, Image, StatusBar, TextInput, ScrollView, FlatList } from 'react-native'
 import DummyData from "./DummyData"
 import Search from '../Assets/Svg/Search.svg'
+import axios from 'axios'
 
 const Home = () => {
     const [search, setSearch] = useState('')
-    const RestaurantCard = () => {
+    const [newYork, setNewYork] = useState([])
+    const [california, setCalifornia] = useState([])
+    const [florida, setFlorida] = useState([])
+    const [error, setError] = useState(false)
+
+    const BreweryCard = ({ item, index }) => {
         return (
-            <>
-                <Text>Cost Effective</Text>
-                <View style={styles.cardContainer}>
-                    <Image
-                        style={styles.cardImage}
-                        source={{
-                            uri: "https://picsum.photos/200/300.jpg"
-                        }}
-                    />
-                    <Text>Joe's Pizza</Text>
-                </View>
-            </>
+            <View style={styles.cardContainer}>
+                <Text style={styles.brewName}>{item?.name}</Text>
+                <Text style={styles.brewName}>{item?.brewery_type.toUpperCase()}</Text>
+                <Text style={styles.brewName}><Text style={styles.heading}>City:</Text> {item?.city}</Text>
+                <Text style={styles.brewName}><Text style={styles.heading}>State:</Text> {item?.state_province}</Text>
+                <Text style={styles.brewName}><Text style={styles.heading}>Country:</Text> {item?.country}</Text>
+                <Text style={styles.brewName}><Text style={styles.heading}>Phone</Text> {item?.phone}</Text>
+                <Text style={styles.brewName}><Text style={styles.heading}>Street:</Text>{item?.street}</Text>
+            </View>
         )
     }
+
+    useEffect(() => {
+        const stateName = ['new_york', 'california', 'florida'].map(item => item)
+        axios.get('https://api.openbrewerydb.org/v1/breweries', {
+            params: {
+                by_state: 'new_york',
+                per_page: 10
+            }
+        }).then((response) => {
+            setNewYork(response?.data)
+        }).catch((e) => {
+            console.log(e)
+            setError(true)
+        })
+        axios.get('https://api.openbrewerydb.org/v1/breweries', {
+            params: {
+                by_state: 'california',
+                per_page: 10
+            }
+        }).then((response) => setCalifornia(response?.data)).catch((e) => {
+            console.log(e)
+            setError(true)
+        })
+        axios.get('https://api.openbrewerydb.org/v1/breweries', {
+            params: {
+                by_state: 'florida',
+                per_page: 10
+            }
+        }).then((response) => setFlorida(response?.data)).catch((e) => {
+            console.log(e)
+            setError(true)
+        })
+    }, [])
+
     return (
         <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
             <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
-            <Text style={styles.title}>Restaurant App</Text>
+            <Text style={styles.title}>Brewery Listing App </Text>
             <View style={styles.searchContainer}>
                 <View style={styles.searchIconConainer}>
                     <Image
@@ -33,14 +70,50 @@ const Home = () => {
                     />
                 </View>
                 <TextInput
-                    placeholder="Search"
+                    editable={false}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    placeholder="Search Using Brewery Types"
                     value={search}
                     onChangeText={setSearch}
                     style={styles.textInput}
-                    onEndEditing={() => { }}
+                // onEndEditing={getBrewery}
                 />
             </View>
-            <RestaurantCard />
+            <Text style={{ marginLeft: '5%', marginTop: '4%' }}>No Search Functionality Yet!</Text>
+            {error ?
+                <Text style={styles.error}>Something Went Wrong!</Text>
+                :
+                <>
+                    <Text style={styles.state}>New York</Text>
+                    <View style={styles.flatListContainer}>
+                        <FlatList
+                            horizontal
+                            data={newYork}
+                            keyExtractor={(_, index) => String(index)}
+                            renderItem={BreweryCard}
+                        />
+                    </View>
+                    <Text style={styles.state}>California</Text>
+                    <View style={styles.flatListContainer}>
+                        <FlatList
+                            horizontal
+                            data={california}
+                            keyExtractor={(_, index) => String(index)}
+                            renderItem={BreweryCard}
+                        />
+                    </View>
+                    <Text style={styles.state}>Florida</Text>
+                    <View style={styles.flatListContainer}>
+                        <FlatList
+                            horizontal
+                            data={florida}
+                            keyExtractor={(_, index) => String(index)}
+                            renderItem={BreweryCard}
+                        />
+                    </View>
+                </>
+            }
         </ScrollView>
     )
 }
@@ -53,7 +126,8 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         color: 'black',
-        margin: '5%'
+        margin: '5%',
+        textAlign: 'center'
     },
     searchContainer: {
         width: '90%',
@@ -94,9 +168,9 @@ const styles = StyleSheet.create({
         height: 200
     },
     cardContainer: {
-        height: 300,
+        // height: 300,
         width: 250,
-        backgroundColor: 'white',
+        backgroundColor: '#0D0D0D',
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -107,8 +181,39 @@ const styles = StyleSheet.create({
 
         elevation: 5,
         borderRadius: 8,
-        overflow: 'hidden'
-    }
+        overflow: 'hidden',
+        marginRight: 10,
+        marginTop: '5%',
+        padding: 5
+    },
+    state: {
+        marginTop: '5%',
+        marginLeft: '5%',
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'black'
+    },
+    brewName: {
+        color: 'white',
+        fontSize: 18,
+        textAlign: 'center',
+        marginTop: 5
+    },
+    heading: {
+        color: '#DE4839',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    flatListContainer: {
+        width: '90%',
+        alignSelf: 'center'
+    },
+    error: {
+        textAlign: 'center',
+        marginTop: '5%',
+        fontSize: 20,
+        color: 'black'
+    },
 })
 
 export default Home
