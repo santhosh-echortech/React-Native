6d3802fe97d1a41f8d149d9a325d777d3da999b5
Santhosh Kumar <santhosh@echortech.com>
2023-04-20 10:05:55 +0530

    Card

--- Screens/Section-8/RestaurantApp/Screens/DummyData.js
+++ Screens/Section-8/RestaurantApp/Screens/DummyData.js
@@ -1,142 +1,142 @@
 export default [
     {
         "name": "Rudy Dare",
-         "price": "$",
+        "price": "$",
         "rating": 49,
         "review_count": 17,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Ronald Brekke",
         "price": '$$',
         "rating": 48,
         "review_count": 3,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Jay Erdman",
         "price": "$",
         "rating": 32,
         "review_count": 52,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Robert Conn",
         "price": "$$",
         "rating": 53,
         "review_count": 2,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Desiree Mills",
         "price": "$$$",
         "rating": 34,
         "review_count": 71,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Erica Pouros",
-        "price":"$$",
+        "price": "$$",
         "rating": 18,
         "review_count": 18,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Julian Gorczany",
         "price": "$$$",
         "rating": 52,
         "review_count": 60,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Joseph Moen",
         "price": "$",
         "rating": 71,
         "review_count": 63,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Cary Skiles",
         "price": "$$$",
         "rating": 84,
         "review_count": 29,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Jesus Blanda",
         "price": "$",
         "rating": 20,
         "review_count": 77,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Sharon Bauch",
         "price": "$$$",
         "rating": 45,
         "review_count": 33,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Terrence Cartwright",
         "price": "$$",
         "rating": 79,
         "review_count": 68,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Vernon Pfeffer",
         "price": "$$",
         "rating": 99,
         "review_count": 58,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Della Block",
         "price": "$",
         "rating": 56,
         "review_count": 94,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Casey Effertz",
         "price": "$$",
         "rating": 74,
         "review_count": 24,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Don Witting",
         "price": '$$',
         "rating": 37,
         "review_count": 1,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Ruby Renner",
         "price": "$",
         "rating": 23,
         "review_count": 68,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Rosalie Okuneva",
         "price": '$$$',
         "rating": 62,
         "review_count": 16,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Tracey O'Kon",
         "price": '$',
         "rating": 25,
         "review_count": 24,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     },
     {
         "name": "Vickie O'Keefe",
         "price": '$$$',
         "rating": 82,
         "review_count": 54,
-        "image_url": ""
+        "image_url": "https://picsum.photos/200/300.jpg"
     }
 ]
--- Screens/Section-8/RestaurantApp/Screens/Home.js
+++ Screens/Section-8/RestaurantApp/Screens/Home.js
@@ -1,24 +1,19 @@
-import React from "react"
-import { View, Text, StyleSheet, Image } from 'react-native'
+import React, { useState } from "react"
+import { View, Text, StyleSheet, Image, StatusBar, TextInput, ScrollView } from 'react-native'
+import DummyData from "./DummyData"
+import Search from '../Assets/Svg/Search.svg'
 
 const Home = () => {
+    const [search, setSearch] = useState('')
     const RestaurantCard = () => {
         return (
             <>
                 <Text>Cost Effective</Text>
-                <View style={{
-                    height: 300,
-                    width: 350,
-                    backgroundColor: 'yellow'
-                }}>
-
+                <View style={styles.cardContainer}>
                     <Image
-                        style={{
-                            width: '100%',
-                            height: 200
-                        }}
+                        style={styles.cardImage}
                         source={{
-                            uri: 'https://example.com/images/restaurant1.jpg',
+                            uri: "https://picsum.photos/200/300.jpg"
                         }}
                     />
                     <Text>Joe's Pizza</Text>
@@ -27,14 +22,93 @@ const Home = () => {
         )
     }
     return (
-        <View>
+        <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+            <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
+            <Text style={styles.title}>Restaurant App</Text>
+            <View style={styles.searchContainer}>
+                <View style={styles.searchIconConainer}>
+                    <Image
+                        style={styles.searchIcon}
+                        source={require('../Assets/Png/Search.png')}
+                    />
+                </View>
+                <TextInput
+                    placeholder="Search"
+                    value={search}
+                    onChangeText={setSearch}
+                    style={styles.textInput}
+                    onEndEditing={() => { }}
+                />
+            </View>
             <RestaurantCard />
-        </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    title: {
+        fontSize: 20,
+        color: 'black',
+        margin: '5%'
+    },
+    searchContainer: {
+        width: '90%',
+        height: 50,
+        alignSelf: 'center',
+        borderRadius: 8,
+        backgroundColor: '#F5F5F5',
+        flexDirection: 'row',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
 
+        elevation: 5,
+    },
+    searchIconConainer: {
+        width: '15%',
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    searchIcon: {
+        width: 30,
+        height: 30
+    },
+    textInput: {
+        height: 50,
+        width: '80%',
+        paddingLeft: '4%',
+        paddingRight: '4%',
+        fontSize: 18
+    },
+    cardImage: {
+        width: '100%',
+        height: 200
+    },
+    cardContainer: {
+        height: 300,
+        width: 250,
+        backgroundColor: 'white',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
+        borderRadius: 8,
+        overflow: 'hidden'
+    }
 })
 
 export default Home
