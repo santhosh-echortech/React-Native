6c6551178d84c59bfaa7fea87b3af06d1326969c
Santhosh Kumar <santhosh@echortech.com>
2023-05-24 01:10:43 +0530

    reducers

--- Screens/Section-17/Animation/Components/LibrariesList.js
+++ Screens/Section-17/Animation/Components/LibrariesList.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { FlatList,Text } from 'react-native'
+import { FlatList, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 
 const LibrariesList = ({ libraries }) => {
     const renderLibraries = (item) => {
         return (
-            <Text>{item?.title}</Text>
+            <TouchableOpacity activeOpacity={0.7} style={styles.row}>
+                <Text style={styles.title}>{item?.title}</Text>
+            </TouchableOpacity>
         )
     }
     return (
@@ -20,5 +22,23 @@ const LibrariesList = ({ libraries }) => {
 const mapStateToProps = state => {
     return { libraries: state.libraries }
 }
+
+const styles = StyleSheet.create({
+    row: {
+        height: 50,
+        width: '90%',
+        alignSelf: 'center',
+        marginBottom: 5,
+        // alignItems:'center',
+        justifyContent: 'center',
+        borderBottomColor:'#A4A4A4',
+        borderBottomWidth:0.5
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '600',
+        color:'black'
+    },
+})
 
 export default connect(mapStateToProps)(LibrariesList)
